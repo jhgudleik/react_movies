e3d9f2665e85554a267edf4db81b2ae4b4861ba9
jhgudleik <jhgudleik@gmail.com>
2026-07-03 14:52:30 +0300

    movies done

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,6 +2,8 @@ import './Main.css';
 import React from 'react';
 import MovieList from '../components/MovieList.js';
 import Preloader from '../components/Preloader.js';
+import Search from '../components/Search';
+
 class Main extends React.Component 
 {
     state = {
@@ -10,14 +12,21 @@ class Main extends React.Component
     componentDidMount() {
         fetch('https://www.omdbapi.com/?apikey=2b2aca6b&s=matrix')
             .then(response => response.json())
-            .then(data => this.setState({ movies: data.Search }));
+            .then(data => this.setState({movies: data.Search}));
     }   
+searchMovie = (str) =>
+{
+    fetch(`https://www.omdbapi.com/?apikey=2b2aca6b&s=${str}`)
+    .then(response => response.json())
+    .then(data => this.setState({movies: data.Search}))
 
+}
   render() {
     const { movies } = this.state;
     return (
       <div className="main">
-        {movies.length ? <MovieList movies={movies} /> : <Preloader />}
+        <Search searchMovie={this.searchMovie}/>
+        {movies == null ? "Error" : movies.length ? <MovieList movies={movies} /> : <Preloader />}
       </div>
     );
   }
